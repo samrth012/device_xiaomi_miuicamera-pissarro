fe4ecfa3cdf6337a6904096b8b18edac13427e8a
Samrth Singh <63335952+samrth012@users.noreply.github.com>
2024-03-18 18:06:21 +0530

    Initial Upload Device-MiuiCamera-Pissarro

---
+++ BoardConfig.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+include vendor/xiaomi/miuicamera-pissarro/BoardConfigVendor.mk
+
+MIUICAMERA_PATH := device/xiaomi/miuicamera-pissarro
+
+# Properties
+TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
